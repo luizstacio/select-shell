19791db50509c4df0eafec25d39790228c78987d
Alex Gray <alex@mrgray.com>
2015-09-16 01:31:37 -0400

    made sample use more advanced options
    sexier output, etc.

--- sample.js
+++ sample.js
@@ -1,11 +1,26 @@
 'use strict';
 
-var list = require('./index')();
+var list = require('./index')(
+  /* Default values */
+  {
+    pointer: '> ',
+    pointerColor: 'red',
+    checked: ' ◉  ',
+		unchecked: ' ◎  ',
+    checkedColor: 'blue',
+    msgCancel: 'No selected options!',
+    msgCancelColor: 'red',
+    multiSelect: true,
+		inverse: true,
+		prepend: true
+  }
+);
+
 var stream = process.stdin;
 
-list.option('One')
-    .option('Two')
-    .option('Three')
+list.option(' One    ')
+    .option(' Two    ')
+    .option(' Three  ')
     .list();
 
 list.on('select', function(options){
@@ -17,3 +32,5 @@ list.on('cancel', function(options){
   console.log('Cancel list, '+ options.length +' options selected');
   process.exit(0);
 });
+
+
